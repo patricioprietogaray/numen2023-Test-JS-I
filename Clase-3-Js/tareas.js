a9887759e9d904e0020e8bfc3f873c112e35edee
Patricio Prieto Garay <93171508+patricioprietogaray@users.noreply.github.com>
2023-08-12 13:55:05 -0300

    resuelto

--- Clase-3-Js/tareas.js
+++ Clase-3-Js/tareas.js
@@ -94,6 +94,10 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(numero <50 && numero > 20) {
+    return true;
+  }
+  return false;
 }
 
 function esEntero(numero) {
