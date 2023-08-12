820094565efea57e69c4185f5af65c7bbed60d29
Patricio Prieto Garay <93171508+patricioprietogaray@users.noreply.github.com>
2023-08-12 16:51:28 -0300

    resuelto

--- Clase-4-Js-parte1/tareas.js
+++ Clase-4-Js-parte1/tareas.js
@@ -42,6 +42,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
+  return array;
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
