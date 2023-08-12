ff25f2141c7e2f1f1556919c8cd1e66ec395090b
Patricio Prieto Garay <93171508+patricioprietogaray@users.noreply.github.com>
2023-08-12 16:06:20 -0300

    resuelto
    dos formas

--- Clase-3-Js/tareas.js
+++ Clase-3-Js/tareas.js
@@ -157,6 +157,13 @@ function esVerdadero(valor) {
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí:
+  // if(valor==true){return "Soy verdadero";}
+  //   else {return "Soy falso";}
+  
+  // con condicion ternaria
+    let respuesta;
+    (valor==true)?respuesta="Soy verdadero":respuesta="Soy falso";
+    return respuesta;
 }
 
 function tieneTresDigitos(numero) {
