bac7e843b2f94a0b8ab5e95dddf6a4c8a3e8cc07
Patricio Prieto Garay <93171508+patricioprietogaray@users.noreply.github.com>
2023-08-13 11:20:04 -0300

    solucion crear gato

--- Clase-4-Js-parte2/tareas.js
+++ Clase-4-Js-parte2/tareas.js
@@ -1,11 +1,22 @@
 // No cambies los nombres de las funciones.
 
 function crearGato(nombre, edad) {
-  // Crear un nuevo objeto con la propiedad "nombre" y el valor definido como el argumento "nombre".
-  // Agrega una propiedad al objeto con el nombre "edad" y usa el valor definido en el argumento "edad"
+  // Crear un nuevo objeto con la propiedad "nombre" y el valor 
+  //definido como el argumento "nombre".
+  // Agrega una propiedad al objeto con el nombre "edad" y 
+  //usa el valor definido en el argumento "edad"
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  const nuevoGato = {
+    nombre: nombre,
+    edad: edad,
+    meow: () => "Meow!"  // funcion flecha
+    // meow: function() {
+    //   return "Meow!";
+    // }
+  }
+  return nuevoGato;
 }
 
 
