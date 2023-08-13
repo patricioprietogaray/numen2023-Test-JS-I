442d20a78a22365bbcc02048efff21e18a8a4069
Patricio Prieto Garay <93171508+patricioprietogaray@users.noreply.github.com>
2023-08-13 11:24:53 -0300

    solucion crear propiedad en objeto

--- Clase-4-Js-parte2/tareas.js
+++ Clase-4-Js-parte2/tareas.js
@@ -25,6 +25,10 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+
+  // crear una propiedad
+  objeto[property]=null;
+  return objeto;
 }
 
 function invocarMetodo(objeto, metodo) {
